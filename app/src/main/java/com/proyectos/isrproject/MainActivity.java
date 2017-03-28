a2edc71ab38c869ecf11e686c4af5fdd444d703a
Chavez, Jonathan <jonachavez@paypal.com>
2017-03-27 19:29:23 -0600

    Match initial firebaseAuth with butterknife and UI

--- app/src/main/java/com/proyectos/isrproject/MainActivity.java
+++ app/src/main/java/com/proyectos/isrproject/MainActivity.java
@@ -4,11 +4,17 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.login_status) TextView loginStatus;
 
     private static final String TAG = "MainActivity";
 
@@ -19,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
         mAuth = FirebaseAuth.getInstance();
         mAuthListener = new FirebaseAuth.AuthStateListener() {
@@ -28,9 +35,11 @@ public class MainActivity extends AppCompatActivity {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
                 if (user != null) {
                     // User is signed in
+                    loginStatus.setText(R.string.signed_in);
                     Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
                 } else {
                     // User is signed out
+                    loginStatus.setText(R.string.signed_out);
                     Log.d(TAG, "onAuthStateChanged:signed_out");
                 }
             }
