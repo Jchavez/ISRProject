1d24bcb8dcf81cbda9e48cd71c54175eb944b7f7
Chavez, Jonathan <jonachavez@paypal.com>
2017-03-28 12:25:30 -0600

    Simple authentication using firebase

--- app/src/main/java/com/proyectos/isrproject/MainActivity.java
+++ app/src/main/java/com/proyectos/isrproject/MainActivity.java
@@ -3,18 +3,26 @@ package com.proyectos.isrproject;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.login_status) TextView loginStatus;
+    @BindView(R.id.login_email) EditText loginEmail;
+    @BindView(R.id.login_password) EditText loginPassword;
 
     private static final String TAG = "MainActivity";
 
@@ -58,5 +66,63 @@ public class MainActivity extends AppCompatActivity {
         if (mAuthListener != null) {
             mAuth.removeAuthStateListener(mAuthListener);
         }
+    }
+
+    private void signIn(String email, String password) {
+        Log.d(TAG, "signIn:" + email);
+        if (!validateForm()) {
+            return;
+        }
+
+        //showProgressDialog();
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+
+                    // If sign in fails, display a message to the user. If sign in succeeds
+                    // the auth state listener will be notified and logic to handle the
+                    // signed in user can be handled in the listener.
+                    if (!task.isSuccessful()) {
+                        Log.w(TAG, "signInWithEmail:failed", task.getException());
+                        /*Toast.makeText(EmailPasswordActivity.this, R.string.auth_failed,
+                            Toast.LENGTH_SHORT).show();*/
+                    }
+
+                    if (!task.isSuccessful()) {
+                        //mStatusTextView.setText(R.string.auth_failed);
+                    }
+                    //hideProgressDialog();
+                }
+            });
+    }
+
+    private boolean validateForm() {
+        boolean valid = true;
+
+        String email = loginEmail.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            loginEmail.setError("Required.");
+            valid = false;
+        } else {
+            loginEmail.setError(null);
+        }
+
+        String password = loginPassword.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            loginPassword.setError("Required.");
+            valid = false;
+        } else {
+            loginPassword.setError(null);
+        }
+
+        return valid;
+    }
+
+    @OnClick(R.id.login_button)
+    public void loginButton() {
+        signIn(loginEmail.getText().toString(), loginPassword.getText().toString());
     }
 }
